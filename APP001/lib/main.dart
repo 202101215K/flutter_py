204bcc736ec4b99d61371e5b4b7e736752d0598b
202101215K <202101215k@utea.edu.pe>
2024-12-05 07:50:52 -0500

    image

--- APP001/lib/main.dart
+++ APP001/lib/main.dart
@@ -9,16 +9,14 @@ void main(List<String> args) {
 
           body: SafeArea(
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                  Icons.ac_unit_outlined,
-                  size: 30.0,
-                ),
-                 Icon(
-                  Icons.accessibility_sharp,
-                  color: Colors.blue,
-                  size: 30.0,
-                )
+                Container(
+                    child: Image.network(
+                  "https://scontent.faqp4-1.fna.fbcdn.net/v/t39.30808-6/318618631_3437777176498910_3368643244247243052_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=9eae26&_nc_eui2=AeHysJVOpJqdgzvcv3V3iF9Ky0CUz77ArWXLQJTPvsCtZaipKF1qerVFgwQESnQozltkysb3ZlybAjhsbwkjEq8b&_nc_ohc=mckouEz_xBYQ7kNvgFZgtwj&_nc_zt=23&_nc_ht=scontent.faqp4-1.fna&_nc_gid=AUt2xgCMZCQYKzL1PfFyZXg&oh=00_AYDZaXtXNW4ZJgcruvFztl9hoZP1BVdNLz9Zt1_yAkb5yw&oe=675764A8",
+                  height: 300,
+                  width: 300,
+                )),
               ],
             ),
           )),
