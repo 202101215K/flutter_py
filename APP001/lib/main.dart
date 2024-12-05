2016dc2c22960769bc99df838a3f728a460caf27
202101215K <202101215k@utea.edu.pe>
2024-12-05 07:11:48 -0500

    column

--- APP001/lib/main.dart
+++ APP001/lib/main.dart
@@ -4,19 +4,38 @@ void main(List<String> args) {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFFc1121f), //Color.fromARGB(255, 28, 10, 224),
+          backgroundColor:
+              Color(0xFFc1121f), //Color.fromARGB(255, 28, 10, 224),
 
-        body: SafeArea(
-            child: Container(
-          color:Colors.blue,
-          child: Text("Hola"),
-          height: 200.0,
-          width: 200.0,
-          margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 50.0),
-          padding: EdgeInsets.all(15.0),
-          alignment: Alignment.center,
-        )),
-      ),
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+
+                Container(
+                  height: 100,
+                  width: 200,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 300,
+                  color: Colors.orange,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          )),
     ),
   );
 }
